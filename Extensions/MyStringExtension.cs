e308e02191577c9dbaa06182233329e9cf80ec57
XyrWL <peter.trullenque@gmail.com>
2015-12-07 14:29:34 +0100

    Extensions.

---
+++ Extensions/MyStringExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions
+{
+    public static class MyStringExtension
+    {
+        public static string Dotify(this string text) //this string : extends string
+        {
+            string result = text.Trim(); //avoid direct changes on text
+            if (result[result.Length - 1] != '.')
+                return result + '.';
+            else
+                return result;
+        }
+    }
+}
---
+++ Extensions/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = MyStringExtension.Dotify("Hej");
+            Console.WriteLine(name);
+            Console.WriteLine("Kalle".Dotify());            
+        }
+    }
+}
