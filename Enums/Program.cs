5b7e2c95e8a3498802c27b6de513ee76917ae1e1
XyrWL <peter.trullenque@gmail.com>
2015-12-14 11:00:22 +0100

    Method extraction on Enums and added EnumExercise in TestExercises.

--- Enums/Program.cs
+++ Enums/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +13,7 @@ namespace Enums
     //Can be written outside or in a class, preferably in its own separate file if the latter though it's acceptable for it to share space in a relevant file.
     enum TestMenu
     {
-        TestCase0, TestCase1, TestCase2
+        Press, Any, Key
     }
     enum Menu
     {
@@ -23,50 +24,81 @@ namespace Enums
         static void Main(string[] args)
         {
             PrintMenuWithEnum(typeof(TestMenu));
-            Console.WriteLine();
+            Console.ReadKey();
+            RunMathProgram();
+        }
 
+        private static void RunMathProgram()
+        {
             bool isEnd = false;
             do
             {
                 Console.Clear();
                 Console.ForegroundColor = ConsoleColor.Blue; //Example of a common enum.
                 PrintMenuWithEnum(typeof(Menu));
-                int option;
-                if (int.TryParse(Console.ReadLine(), out option))
-                    //Returns true if the parse was successful, result goes to out parameter.
+                MenuChoice(ref isEnd);
+            }
+            while (!isEnd);
+        }
+
+        private static void MenuChoice(ref bool isEnd)
+        {
+            int option;
+            if (int.TryParse(Console.ReadLine(), out option))
+            //Returns true if the parse was successful, result goes to out parameter.
+            {
+                Menu optionMenu = (Menu)option;
+                switch (optionMenu)
                 {
-                    Menu optionMenu = (Menu) option;
-                    switch (optionMenu)
-                    {
-                        case Menu.Add:
-                            Console.WriteLine(Menu.Add); //ToString() method on enum returns the name, not the value.
-                            break;
-                        case Menu.Substract:
-                            Console.WriteLine(Menu.Substract);
-                            break;
-                        case Menu.Multiply:
-                            Console.WriteLine(Menu.Multiply);
-                            break;
-                        case Menu.Divide:
-                            Console.WriteLine(Menu.Divide);
-                            break;
-                        case Menu.End:
-                            Console.WriteLine(Menu.End);
-                            isEnd = true;
-                            break;
-                        default:
-                            //throw new ArgumentOutOfRangeException();
-                            Console.WriteLine("Something went wrong, try again.");
-                            break;
-                    }
-                    System.Threading.Thread.Sleep(3000);
+                    case Menu.Add:
+                        ConsoleAddMethod();
+                        PrintConsoleWait();
+                        break;
+                    case Menu.Substract:
+                        break;
+                    case Menu.Multiply:
+                        break;
+                    case Menu.Divide:
+                        break;
+                    case Menu.End:
+                        Console.WriteLine(Menu.End); //ToString() method on enum returns the name, not the value.
+                        isEnd = true;
+                        break;
+                    default:
+                        //throw new ArgumentOutOfRangeException();
+                        Console.WriteLine("Something went wrong, try again.");
+                        PrintConsoleWait();
+                        break;
                 }
-            } while (!isEnd);
+            }
+        }
+
+        private static void PrintConsoleWait()
+        {
+            Console.Write(".");
+            System.Threading.Thread.Sleep(1000);
+            Console.Write(".");
+            System.Threading.Thread.Sleep(1000);
+            Console.Write(".");
+            System.Threading.Thread.Sleep(1000);
+        }
+
+        private static void ConsoleAddMethod()
+        {
+            //Example method, should have better input control, separate input and output from result calculation.
+            Console.WriteLine("{0}:", Menu.Add);
+            int firstNumber;
+            int secondNumber;
+            if (int.TryParse(Console.ReadLine(), out firstNumber) &&
+                int.TryParse(Console.ReadLine(), out secondNumber))
+            {
+                Console.WriteLine("{0} + {1} = {2}", firstNumber, secondNumber, firstNumber + secondNumber);
+            }
         }
 
         private static void PrintMenuWithEnum(Type typeOfEnum)
         {
-            int counter = (int) Enum.GetValues(typeOfEnum).GetValue(0);
+            int counter = (int)Enum.GetValues(typeOfEnum).GetValue(0);
             foreach (var item in Enum.GetNames(typeOfEnum)) //Turns the enum into a string array.
             {
                 Console.WriteLine("{0}: {1}", counter++, item);
--- TestExercises/Program.cs
+++ TestExercises/Program.cs
@@ -1,20 +1,130 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TestExercises
 {
+    enum FirstMenu
+    {
+        Ett = 1, Tvau, Avsluta
+    }
+    enum SecondMenu
+    {
+        Lista, Räkna, Avsluta
+    }
+    enum ThirdMenu
+    {
+        Create, Restore, Find, Exit
+    }
+
     class Program
     {
         static void Main(string[] args)
+        {
+            //ListExercise();
+            EnumExercise();
+        }
+
+        private static void EnumExercise()
+        {
+            PrintMenuWithEnum(typeof(FirstMenu));
+            SelectOptionFromFirstMenu();
+            PrintMenuWithEnum(typeof(SecondMenu));
+            SelectOptionFromSecondMenu();
+            PrintMenuWithEnum(typeof(ThirdMenu));
+            SelectOptionFromThirdMenu();
+        }
+        private static void SelectOptionFromThirdMenu()
+        {
+            int option;
+            if (int.TryParse(Console.ReadLine(), out option))
+            {
+                ThirdMenu optionMenu = (ThirdMenu)option;
+                switch (optionMenu)
+                {
+                    case ThirdMenu.Create:
+                        Console.WriteLine(ThirdMenu.Create);
+                        break;
+                    case ThirdMenu.Restore:
+                        Console.WriteLine(ThirdMenu.Restore);
+                        break;
+                    case ThirdMenu.Find:
+                        Console.WriteLine(ThirdMenu.Find);
+                        break;
+                    case ThirdMenu.Exit:
+                        Console.WriteLine(ThirdMenu.Exit);
+                        break;
+                    default:
+                        Console.WriteLine("Wrong input.");
+                        break;
+                }
+            }
+        }
+        private static void SelectOptionFromSecondMenu()
+        {
+            int option;
+            if (int.TryParse(Console.ReadLine(), out option))
+            {
+                SecondMenu optionMenu = (SecondMenu) option;
+                switch (optionMenu)
+                {
+                    case SecondMenu.Lista:
+                        Console.WriteLine(SecondMenu.Lista);
+                        break;
+                    case SecondMenu.Räkna:
+                        Console.WriteLine(SecondMenu.Räkna);
+                        break;
+                    case SecondMenu.Avsluta:
+                        Console.WriteLine(SecondMenu.Avsluta);
+                        break;
+                    default:
+                        Console.WriteLine("Wrong input.");
+                        break;
+                }
+            }
+        }
+        private static void SelectOptionFromFirstMenu()
+        {
+            int option;
+            if (int.TryParse(Console.ReadLine(), out option))
+            {
+                FirstMenu optionMenu = (FirstMenu) option;
+                switch (optionMenu)
+                {
+                    case FirstMenu.Ett:
+                        Console.WriteLine(FirstMenu.Ett);
+                        break;
+                    case FirstMenu.Tvau:
+                        Console.WriteLine(FirstMenu.Tvau);
+                        break;
+                    case FirstMenu.Avsluta:
+                        Console.WriteLine(FirstMenu.Avsluta);
+                        break;
+                    default:
+                        Console.WriteLine("Wrong input.");
+                        break;
+                }
+            }
+        }
+        private static void PrintMenuWithEnum(Type typeOfEnum)
+        {
+            int counter = (int) Enum.GetValues(typeOfEnum).GetValue(0);
+            foreach (var item in Enum.GetNames(typeOfEnum)) //Turns the enum into a string array.
+            {
+                Console.WriteLine("{0}: {1}", counter++, item);
+            }
+        }
+
+        private static void ListExercise()
         {
             var firstProduct = new Product(name: "Orange", quantity: 7);
             var secondProduct = new Product(name: "Apple", quantity: 59);
             var thirdProduct = new Product(name: "Rabbit", quantity: 12);
 
-            var productList = new List<Product>() { firstProduct, secondProduct, thirdProduct };
+            var productList = new List<Product>() {firstProduct, secondProduct, thirdProduct};
             productList.Sort();
 
             Console.WriteLine("Minimum amount?");
